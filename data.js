8e048580648bf63440005bc417a5de9fe0b77905
Caden <mcaden966@gmail.com>
2021-01-04 19:29:39 -0600

    finished the day page

---
+++ data.js
@@ -0,0 +1,20 @@
+let velocityDayData = [];
+velocityDayData.push({Exercise: "PlyoCare Reverse Throws", Reps: "No more than 10 reps with 1 kg AND 2kg balls"});
+velocityDayData.push({Exercise: "PlyoCare Pivot Pickoffs", Reps: "No more than 10 reps with 1 kg AND 2kg balls"});
+velocityDayData.push({Exercise: "PlyoCare Roll-In Throws", Reps: "No more than 10 reps with 1 kg AND 450g balls"});
+velocityDayData.push({Exercise: "PlyoCare Rocker Throws", Reps: "1 throw each with blue, red, yellow, gray"});
+velocityDayData.push({Exercise: "PlyoCare Walking Windup", Reps: "1 throw each with blue, red, yellow, gray"});
+velocityDayData.push({Exercise: "Jaeger Long Toss Series", Reps: "Extension Phase/Light Catch"});
+velocityDayData.push({Exercise: "Run and Gun, Weighted Balls", Reps: "3-4 throws each (5, 6, 7, 5, 4, 3); first throw at 80%"});
+velocityDayData.title = "Velocity Day";
+
+let plyoCareVelocityData = [];
+plyoCareVelocityData.push({Exercise: "PlyoCare Reverse Throws", Reps: "10 reps with 1 kg AND 2kg balls"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Reverse Throws (Optional)", Reps: "10 reps with 1 kg AND 2kg balls"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Pivot Pickoffs", Reps: "10 reps with 1 kg AND 2kg balls"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Roll-In Throws", Reps: "3 reps with 1 kg ball, 5 reps with 450g ball"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Rocker Throws", Reps: "1 throw each with blue, red, yellow, gray"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Rocker Throws", Reps: "1 throw each with blue, red, yellow, gray"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Walking Windup", Reps: "1 throw each with blue, red, yellow, gray"});
+plyoCareVelocityData.push({Exercise: "PlyoCare Walking Windup", Reps: "1 throw each with blue, red, yellow, gray"});
+plyoCareVelocityData.title = "PlyoCare Velocity";
--- pages.js
+++ pages.js
@@ -1,6 +1,12 @@
+let hasBeenInc = false;
+let numClicked = 0;
+
 createHomePage();
 
 function createHomePage() {
+    hasBeenInc = false;
+    numClicked = 0; 
+
     let title = document.createElement("h1");
     title.id = "Title";
     title.innerHTML = "Off-Season Throwing Program";
@@ -14,11 +20,13 @@ function createHomePage() {
     velocityDayButton.id = "Velocity Day";
     velocityDayButton.className = "button";
     velocityDayButton.innerHTML = "Velocity Day";
+    velocityDayButton.addEventListener("click", function() { deleteHomePage(); createDayPage(velocityDayData); });
 
     let plyoCareVelocity = document.createElement("button");
     plyoCareVelocity.id = "PlyoCare Velocity";
     plyoCareVelocity.className = "button";
     plyoCareVelocity.innerHTML = "PlyoCare Velocity";
+    plyoCareVelocity.addEventListener("click", function() { deleteHomePage(); createDayPage(plyoCareVelocityData); });
 
     let hybridA = document.createElement("button");
     hybridA.id = "Hybrid A";
@@ -42,4 +50,78 @@ function createHomePage() {
     document.body.appendChild(hybridA);
     document.body.appendChild(hybridB);
     document.body.appendChild(recovery);
+}
+
+function deleteHomePage() {
+    document.getElementById("Title").remove();
+    document.getElementById("overview").remove();
+    document.getElementById("Velocity Day").remove();
+    document.getElementById("PlyoCare Velocity").remove();
+    document.getElementById("Hybrid A").remove();
+    document.getElementById("Hybrid B").remove();
+    document.getElementById("Recovery").remove();
+}
+
+function createDayPage(day) {
+    let back = document.createElement("button");
+    back.id = "back";
+    back.className = "back";
+    back.innerHTML = "Back";
+    back.addEventListener("click", function() { deleteDayPage(); createHomePage(); });
+
+    let title = document.createElement("h1");
+    title.id = "Title";
+    title.innerHTML = day.title;
+
+    let tableContainer = document.createElement("table");
+    tableContainer.id = "tableContainer";
+    tableContainer.className = "table_container";
+
+    let table = document.createElement("table");
+    tableContainer.appendChild(table);
+
+    for (let i = 0; i < day.length; i++) {
+        let label = document.createElement ("label");
+		label.id = "label" + i;
+		label.className = "switch";
+		let input = document.createElement ("input");
+		input.type = "checkbox";
+		input.addEventListener ("change", function () {
+			if (input.checked === true && hasBeenInc === false) {
+				numClicked++;
+			}
+			else if (input.checked === false && hasBeenInc === false) {
+				numClicked--
+			}
+
+			if (numClicked === day.length && hasBeenInc === false) {
+				hasBeenInc = true;
+				window.alert ("Completed!");
+			}
+		});
+		let span = document.createElement ("span");
+		span.className = "slider";
+
+		label.appendChild (input);
+        label.appendChild (span);
+        
+        let row = table.insertRow(i);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+
+        cell1.innerHTML = day[i].Exercise;
+        cell2.innerHTML = day[i].Reps;
+        cell3.appendChild(label);
+    }
+
+    document.body.appendChild(back);
+    document.body.appendChild(title);
+    document.body.appendChild(tableContainer);
+}
+
+function deleteDayPage() {
+    document.getElementById("back").remove();
+    document.getElementById("Title").remove();
+    document.getElementById("tableContainer").remove();
 }
