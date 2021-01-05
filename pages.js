2e4161e9ec262641f5e152bd96a24fa8aaf31507
Caden <mcaden966@gmail.com>
2021-01-04 18:16:35 -0600

    added buttons for each type of day

--- pages.js
+++ pages.js
@@ -10,6 +10,36 @@ function createHomePage() {
     img.src = "overview.png";
     img.alt = "overview";
 
+    let velocityDayButton = document.createElement("button");
+    velocityDayButton.id = "Velocity Day";
+    velocityDayButton.className = "button";
+    velocityDayButton.innerHTML = "Velocity Day";
+
+    let plyoCareVelocity = document.createElement("button");
+    plyoCareVelocity.id = "PlyoCare Velocity";
+    plyoCareVelocity.className = "button";
+    plyoCareVelocity.innerHTML = "PlyoCare Velocity";
+
+    let hybridA = document.createElement("button");
+    hybridA.id = "Hybrid A";
+    hybridA.className = "button";
+    hybridA.innerHTML = "Hybrid A";
+
+    let hybridB = document.createElement("button");
+    hybridB.id = "Hybrid B";
+    hybridB.className = "button";
+    hybridB.innerHTML = "Hybrid B";
+
+    let recovery = document.createElement("button");
+    recovery.id = "Recovery";
+    recovery.className = "button";
+    recovery.innerHTML = "Recovery";
+
     document.body.appendChild(title);
     document.body.appendChild(img);
+    document.body.appendChild(velocityDayButton);
+    document.body.appendChild(plyoCareVelocity);
+    document.body.appendChild(hybridA);
+    document.body.appendChild(hybridB);
+    document.body.appendChild(recovery);
 }
